4635ab95b71cdfe70e23e934e224e3a3f05a7e04
Ryan <ryan@ebbflow.io>
2020-06-29 19:02:05 -0700

    Update per moderator comments

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,26 +2,20 @@
 
 $packageName= 'ebbflow'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url64bit      = 'https://github.com/ebbflow-io/ebbflow/releases/download/0.5.20/ebbflow_0.5.20.msi' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  url64bit      = 'https://github.com/ebbflow-io/ebbflow/releases/download/0.9.0/ebbflow_0.9.0.msi' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
   softwareName  = 'ebbflow*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum64    = '0FB77CFB0428524C3DDC624CB6EE9B5E22F73FEC81A73CA562277E5C80492A4F'
+  checksum64    = 'A26AED4FC721B07E17560589DDD983CA3685FCDA8BEF3336A56DB02AF169F58E'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
 }
 
-#https://chocolatey.org/docs/helpers-install-chocolatey-package
 Install-ChocolateyPackage @packageArgs
-## If you are making your own internal packages (organizations), you can embed the installer or
-## put on internal file share and use the following instead (you'll need to add $file to the above)
-# https://chocolatey.org/docs/helpers-install-chocolatey-install-package
-#Install-ChocolateyInstallPackage @packageArgs
