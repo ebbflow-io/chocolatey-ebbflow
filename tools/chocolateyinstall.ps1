8a9c92a7627f92c8ffcc6b4c2ab9c2301f2c83a1
Ryan <ryan@ebbflow.io>
2020-06-30 13:58:52 -0700

    Go back to version 0.5.20 just for chocolatey

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url64bit      = 'https://github.com/ebbflow-io/ebbflow/releases/download/0.9.0/ebbflow_0.9.0.msi' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  url64bit      = 'https://github.com/ebbflow-io/ebbflow/releases/download/0.5.20/ebbflow_0.5.20.msi' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
   softwareName  = 'ebbflow*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum64    = 'A26AED4FC721B07E17560589DDD983CA3685FCDA8BEF3336A56DB02AF169F58E'
+  checksum64    = '0FB77CFB0428524C3DDC624CB6EE9B5E22F73FEC81A73CA562277E5C80492A4F'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
