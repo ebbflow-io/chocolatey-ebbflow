50be9a9619386d1a5a4e1b9adb39745040874a5c
Ryan <ryan@ebbflow.io>
2020-07-02 10:54:33 -0700

    Update some URLs and remove comments

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,14 +7,14 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url64bit      = 'https://github.com/ebbflow-io/ebbflow/releases/download/0.5.20/ebbflow_0.5.20.msi' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+  url64bit      = 'https://github.com/ebbflow-io/ebbflow/releases/download/0.5.20/ebbflow_0.5.20.msi'
 
-  softwareName  = 'ebbflow*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'ebbflow*'
 
   checksum64    = '0FB77CFB0428524C3DDC624CB6EE9B5E22F73FEC81A73CA562277E5C80492A4F'
   checksumType64= 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
